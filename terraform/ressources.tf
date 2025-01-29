8f26592a2e80384ed3d4d386985c8a65911b3ae4
Pia <piaheiss@gmx.de>
2025-01-29 11:45:35 +0100

    fix: ansible Workflow

--- terraform/ressources.tf
+++ terraform/ressources.tf
@@ -1,7 +1,7 @@
 
 # EC2-Instanz erstellen und Docker installieren
 resource "aws_instance" "docker_instance" {
-  ami           = "ami-07eef52105e8a2059" # Amazon Linux 2 AMI ID (Region anpassen)
+  ami           = "ami-07eef52105e8a2059" # Ubuntu
   instance_type = "t2.micro"             # Instanztyp anpassen
   
   vpc_security_group_ids = [aws_security_group.docker_sg.id]
