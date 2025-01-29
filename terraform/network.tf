5fefc18f942dd0cf1d1998e1dc30f55f7a1b7291
Pia <piaheiss@gmx.de>
2025-01-29 09:22:55 +0100

    add: templates for ansible

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,24 +1,3 @@
-# VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
-    Name = "Docker-React"
-  }
-}
-
-
-# Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Docker-React VPC IG"
-  }
-}
-
 
 # Sicherheitsgruppe erstellen
 resource "aws_security_group" "docker_sg" {
