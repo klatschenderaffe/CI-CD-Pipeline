edce48686f0a3aa8fee24037c34a79f0c587b064
Pia <piaheiss@gmx.de>
2025-01-29 11:12:59 +0100

    fix: ansible Workflow

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,5 @@
 
 # Ausgabe der öffentlichen IP-Adresse der EC2-Instanz
 output "public_ips" {
-  value = aws_instance.docker_instance.public_ip
+  value = aws_instance.docker_instance.*.public_ip
 }
